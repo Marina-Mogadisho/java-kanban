4d7ddeb648d97acc68aedfc1d2778d27b24512b4
marina <shadrinam2012@yandex.ru>
2025-03-02 00:47:42 +0300

    fead - новый функционал, ТЗ спринта 9 (2)

--- src/util/UtilHttp.java
+++ src/util/UtilHttp.java
@@ -6,17 +6,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class UtilHttp {
-    public static ResponseClient send(String type, String UrlRequest,Integer id,String suffix ,String body){
-        return send(type, UrlRequest+id+suffix, body);
+    public static ResponseClient send(String type, String UrlRequest, Integer id, String suffix, String body) {
+        return send(type, UrlRequest + id + suffix, body);
     }
-    public static ResponseClient send(String type, String UrlRequest,Integer id, String body){
-        return send(type, UrlRequest+id, body);
+
+    public static ResponseClient send(String type, String UrlRequest, Integer id, String body) {
+        return send(type, UrlRequest + id, body);
     }
-    public static ResponseClient send(String type, String UrlRequest, String body){
-        //HttpClient client = HttpClient.newHttpClient();
+
+    public static ResponseClient send(String type, String UrlRequest, String body) {
         URI url = URI.create(UrlRequest);
-        HttpRequest request=null;
-        switch(type) {
+        HttpRequest request = null;
+        switch (type) {
             case "POST":
                 request = HttpRequest.newBuilder()
                         .uri(url)
@@ -41,15 +42,14 @@ public class UtilHttp {
                         .build();
                 break;
         }
-        if(request==null)return new ResponseClient(500,"Error type:"+type);
+        if (request == null) return new ResponseClient(500, "Error type:" + type);
         //отправляем запрос клиента (request) на сервер и получаем ответ - response
 
-        try(HttpClient client = HttpClient.newHttpClient()){
+        try (HttpClient client = HttpClient.newHttpClient()) {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            return new ResponseClient(response.statusCode(),response.body());
-        }
-        catch(Exception e){
-            return new ResponseClient(500,"Error open URL:"+UrlRequest);
+            return new ResponseClient(response.statusCode(), response.body());
+        } catch (Exception e) {
+            return new ResponseClient(500, "Error open URL:" + UrlRequest);
         }
     }
 }
